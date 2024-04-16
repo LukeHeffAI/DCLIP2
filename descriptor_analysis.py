14de2e4cb0b15127d3ec47b487d95c06b392c6bb
Luke Heffernan <luke.heffernan@adelaide.edu.au>
2024-04-16 14:02:41 +0930

    Refactor descriptor analysis functions

--- descriptor_analysis.py
+++ descriptor_analysis.py
@@ -1,19 +1,32 @@
 import torch
 import json
 import time as t
-import seaborn as sns
 from loading_helpers import load_json
+
+def compute_descriptor_list(data):
+    '''
+    Compute the list of all descriptors in the data.
+    '''
+    descriptor_list = []
+    for v in data.values():
+        descriptor_list.extend(v)
+    return descriptor_list
+
+def compute_class_list(data):
+    '''
+    Compute the list of all classes in the data.
+    '''
+    class_list = []
+    for k in data.keys():
+        class_list.append(k)
+    return class_list
 
 def compute_freq_is(data):
     '''
     Compute the frequency of each value in the data when the value completely matches the key.    
     '''
-    class_list = []
-    descriptor_list = []
-    descriptor_frequencies = {}
-    for k, v in data.items():
-        class_list.append(k)
-        descriptor_list.extend(v)
+
+    descriptor_list = compute_descriptor_list(data)
     descriptor_frequencies = {i: 0 for i in descriptor_list}
     for entry in descriptor_list:
         if entry in descriptor_frequencies:
@@ -25,9 +38,7 @@ def compute_freq_contains(data):
     '''
     Compute the frequency of each item in the value list in the data, including when the item is a substring of another item.
     '''
-    descriptor_list = []
-    for v in data.values():
-        descriptor_list.extend(v)
+    descriptor_list = compute_descriptor_list(data)
     descriptor_frequencies = {}
     for descriptor in descriptor_list:
         if descriptor in descriptor_frequencies:
@@ -62,26 +73,26 @@ for json_path in descriptor_file:
     with open(json_output_path, 'w') as f:
         json.dump(freq, f, indent=4)
 
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+# import pandas as pd
+# import seaborn as sns
+# import matplotlib.pyplot as plt
 
-df_is = pd.DataFrame([(k, v) for k, v in freq_is.items() if v > 6], columns=['Value', 'Frequency'])
-df_is = df_is.sort_values(by='Frequency', ascending=False)
-# df_contains = pd.DataFrame([(k, v) for k, v in freq_contains.items() if v > 15], columns=['Value', 'Frequency'])
-# df_contains = df_contains.sort_values(by='Frequency', ascending=False)
+# df_is = pd.DataFrame([(k, v) for k, v in freq_is.items() if v > 6], columns=['Value', 'Frequency'])
+# df_is = df_is.sort_values(by='Frequency', ascending=False)
+# # df_contains = pd.DataFrame([(k, v) for k, v in freq_contains.items() if v > 15], columns=['Value', 'Frequency'])
+# # df_contains = df_contains.sort_values(by='Frequency', ascending=False)
 
-plt.figure(figsize=(15, 6))  # Adjust the size of the plot as needed
-sns.barplot(x='Value', y='Frequency', data=df_is, palette='viridis')
-# sns.barplot(x='Value', y='Frequency', data=df_contains, palette='viridis')
+# plt.figure(figsize=(15, 6))  # Adjust the size of the plot as needed
+# sns.barplot(x='Value', y='Frequency', data=df_is, palette='viridis')
+# # sns.barplot(x='Value', y='Frequency', data=df_contains, palette='viridis')
 
-# Adding plot title and labels for clarity
-plt.title('Frequency Distribution of Descriptors')
-plt.xlabel('Value')
-plt.ylabel('Frequency')
+# # Adding plot title and labels for clarity
+# plt.title('Frequency Distribution of Descriptors')
+# plt.xlabel('Value')
+# plt.ylabel('Frequency')
 
-# Rotating the x-axis labels for better readability if necessary
-plt.xticks(rotation=60)
+# # Rotating the x-axis labels for better readability if necessary
+# plt.xticks(rotation=60)
 
-# Show the plot
-plt.show()
+# # Show the plot
+# plt.show()
