0814604ab3a331a2b48de42af57f042efb1d5909
Luke Heffernan <luke.heffernan@adelaide.edu.au>
2024-08-05 14:32:42 +0930

    refactor: generate descriptors using OpenAI API instruct models

--- generate_descriptors.py
+++ generate_descriptors.py
@@ -50,6 +50,15 @@ def obtain_descriptors_and_save(filename, class_list):
 
     prompts = [generate_prompt(category.replace('_', ' ')) for category in class_list]
     
+    # responses = [openai.Completion.create(model="gpt3.5-turbo-instruct",
+    #                                         prompt=prompt_partition,
+    #                                         temperature=0.,
+    #                                         max_tokens=100,
+    #                                         ) for prompt_partition in partition(prompts, 20)]
+    # response_texts = [r["text"] for resp in responses for r in resp['choices']]
+    # descriptors_list = [stringtolist(response_text) for response_text in response_texts]
+    # descriptors = {cat: descr for cat, descr in zip(class_list, descriptors_list)}
+
     for prompt_partition in partition(prompts, 20):
         for attempt in range(4):  # Try up to four times (initial + three retries)
             try:
