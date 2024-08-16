7c51b59992bf75e97f372d2d6f1dbe734fe9eeee
Luke Heffernan <luke.heffernan@adelaide.edu.au>
2024-08-16 18:29:56 +0930

    Update classwise print statement for accuracy

--- main.py
+++ main.py
@@ -199,4 +199,4 @@ print("Total Description-based Top-1 Accuracy: ", 100 * overall_lang_accuracy_me
 print("Total Description-based Top-5 Accuracy: ", 100 * overall_lang_accuracy_metric_top5.compute().item(), "%")
 print("Total CLIP-Standard Top-1 Accuracy: ", 100 * overall_clip_accuracy_metric.compute().item(), "%")
 print("Total CLIP-Standard Top-5 Accuracy: ", 100 * overall_clip_accuracy_metric_top5.compute().item(), "%")
-print("Class-wise Accuracies and Differences (Top 5 and Bottom 5):\n", list(sorted_class_wise_accuracies.keys())[:10], "\n", list(sorted_class_wise_accuracies.keys())[-10:])
+print("Class-wise Accuracies and Differences (Top 10 and Bottom 10):\n", list(sorted_class_wise_accuracies.keys())[:10], "\n", list(sorted_class_wise_accuracies.keys())[-10:])
