4fea0a8c6a9a308a1f8fa0592bb80bd5251491bb
LukeHeffAI <luke.heffernan@adelaide.edu.au>
2024-09-21 15:48:19 +0930

    Update load.py to include after_text in hparams and modify descriptor_fname path

--- load.py
+++ load.py
@@ -83,7 +83,7 @@ elif hparams['model_size'] == 'RN50x64' and hparams['image_size'] != 448:
 hparams['before_text'] = ""
 hparams['label_before_text'] = ""
 hparams['between_text'] = ', '
-# hparams['after_text'] = ''
+hparams['after_text'] = ''
 # hparams['between_text'] = ' '
 # hparams['between_text'] = ''
 hparams['unmodify'] = True
@@ -217,7 +217,7 @@ if hparams['dataset'] != 'imagenetv2':
 else:
     dataset_classes = classes_to_load
     
-hparams['after_text'] = f', as an image from the {hparams["dataset_name"]} dataset.'
+# hparams['after_text'] = f', as an image from the {hparams["dataset_name"]} dataset.'
 hparams['descriptor_fname'] = f'./descriptors/{hparams['desc_type']}/{hparams['descriptor_fname']}'
 hparams['descriptor_analysis_fname'] = './descriptor_analysis/descriptors_' + hparams['analysis_fname']
 hparams['class_analysis_fname'] = './class_analysis/json/class_' + hparams['analysis_fname']
