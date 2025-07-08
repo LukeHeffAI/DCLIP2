9259e37e48bac26937d5da5acc12faed89b1ac81
LukeHeffAI <luke.heffernan@adelaide.edu.au>
2025-07-08 09:34:23 +0930

    Refactor hyperparameter setting in generate_descriptors.py to remove update_hparams function

--- generate_descriptors.py
+++ generate_descriptors.py
@@ -6,14 +6,13 @@ import json
 import time
 import itertools
 from descriptor_strings import stringtolist
-from load import set_hparams, update_hparams
+from load import set_hparams
 from loading_helpers import compute_class_list, load_json
 import pathlib
 from torchvision.datasets import ImageFolder
 
 # Set and update the hyperparameters
-hparams = set_hparams(model_size='ViT-B/32', desc_type='gpt4o', dataset='places365', method='clip')
-hparams, tfms, dataset, dataset_classes, class_subcategories, class_subcategory_desc, gpt_descriptions, unmodify_dict, label_to_classname, n_classes = update_hparams(hparams)
+hparams, tfms, dataset_loader, dataset_classes, class_subcategories, gpt_descriptions, unmodify_dict, label_to_classname, n_classes = set_hparams(model_size='ViT-B/32', desc_type='gpt4o', dataset='places365', method='clip')
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
